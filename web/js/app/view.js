ae4d9311427246e34679fde15a15f821a31ec69f
Christian Carey <christian.carey@gmail.com>
2012-11-03 15:37:41 -0700

    working on getting the 'app' views working

--- web/js/app/view.js
+++ web/js/app/view.js
@@ -1,7 +1,26 @@
-var AppView = Backbone.View.extend({
+var UserView = Backbone.View.extend({
+    el: "#header",
+    
+    initialize: function() {
+        _.bindAll(this, "render");
+        this.model.bind("change", this.render);
+    },
+    
     render: function() {
-        this.el = ich.header(this.model.user.toJSON());
-        this.delegateEvents(); // reattach events since we reset this.el
+        console.log(this.model.get("first_name"));
+        $(this.el).html(ich.header_frag(this.model.toJSON()));
+        // this.delegateEvents(); // reattach events since we reset this.el
         return this;
-    }
+    },
+});
+
+var AppView = Backbone.View.extend({
+    initialize: function() {
+        this.userView = new UserView({model:this.model.user});
+    },
+    
+    render: function() {
+        //this.el = this.userView.render().el;
+        return this;
+    },
 });
