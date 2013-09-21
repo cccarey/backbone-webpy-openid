73af37ca89cbe146686e1c2238388ca3da811d98
Christian Carey <christian.carey@gmail.com>
2013-09-21 11:00:43 -0700

    change names of scripts to not include 'min'

--- scripts/get-js-libs.sh
+++ scripts/get-js-libs.sh
@@ -4,7 +4,7 @@ getfile() {
     mv $2 web/js/lib
 }
 
-getfile "http://backbonejs.org/backbone-min.js" "backbone-min.js"
-getfile "http://documentcloud.github.com/underscore/underscore-min.js" "underscore-min.js"
-getfile "http://code.jquery.com/jquery.min.js" "jquery.min.js"
-getfile "https://github.com/andyet/ICanHaz.js/raw/master/ICanHaz.min.js" "ICanHaz.min.js"
+getfile "http://backbonejs.org/backbone-min.js" "backbone.js"
+getfile "http://documentcloud.github.com/underscore/underscore-min.js" "underscore.js"
+getfile "http://code.jquery.com/jquery.min.js" "jquery.js"
+getfile "https://github.com/andyet/ICanHaz.js/raw/master/ICanHaz.min.js" "ICanHaz.js"
