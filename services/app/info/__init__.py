56e57c68155586569eb032585691c95d9711b32b
Christian Carey <christian.carey@gmail.com>
2014-08-16 13:00:10 -0700

    remove old services

--- services/app/info/__init__.py
+++
@@ -1,7 +0,0 @@
-import web, config
-
-class root:
-    def GET(self):
-        web.header('Content-Type', 'application/json')
-        return "{ \"version\": \"%s\" }" % config.settings["version"]
-
